a03942203c1eb7b5fab8b03ade3ccb68460c35b1
Rynco Maekawa <lynzrand@outlook.com>
2019-12-02 21:25:46 +0800

    Add footer

--- src/components/base-components/base-components.module.ts
+++ src/components/base-components/base-components.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SearchbarComponent } from './searchbar/searchbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [NavbarComponent, SearchbarComponent],
+  declarations: [NavbarComponent, SearchbarComponent, FooterComponent],
   imports: [CommonModule],
-  exports: [NavbarComponent, SearchbarComponent],
+  exports: [NavbarComponent, SearchbarComponent, FooterComponent],
 })
 export class BaseComponentsModule {}
