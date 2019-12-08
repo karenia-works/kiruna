979b618cbd2d9ad08448bd2c6cb15c5e9a788bd1
Rynco Maekawa <lynzrand@outlook.com>
2019-12-08 16:49:40 +0800

    Add animation

--- src/components/base-components/base-components.module.ts
+++ src/components/base-components/base-components.module.ts
@@ -3,10 +3,22 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SearchbarComponent } from './searchbar/searchbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { ArticleAbstractComponent } from './article-abstract/article-abstract.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [NavbarComponent, SearchbarComponent, FooterComponent],
-  imports: [CommonModule],
-  exports: [NavbarComponent, SearchbarComponent, FooterComponent],
+  declarations: [
+    NavbarComponent,
+    SearchbarComponent,
+    FooterComponent,
+    ArticleAbstractComponent,
+  ],
+  imports: [CommonModule, RouterModule],
+  exports: [
+    NavbarComponent,
+    SearchbarComponent,
+    FooterComponent,
+    ArticleAbstractComponent,
+  ],
 })
 export class BaseComponentsModule {}
