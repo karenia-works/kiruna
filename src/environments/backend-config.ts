81c4c0eebb51a9336cd34cbeb920e982f3889283
Rynco Maekawa <lynzrand@outlook.com>
2019-12-13 15:34:45 +0800

    prepare for backend

---
+++ src/environments/backend-config.ts
@@ -0,0 +1,8 @@
+export const backendConfig = {
+  endpoints: {
+    account: {
+      login: '/account/login',
+      info: '/account/info/{id}',
+    },
+  },
+};
--- src/services/account/account.service.ts
+++ src/services/account/account.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class AccountService {
@@ -7,4 +8,12 @@ export class AccountService {
    *
    */
   constructor(private httpClient: HttpClient) {}
+
+  login(username: string, password: string) {
+    this.httpClient.post();
+  }
+}
+
+export interface UserInfo {
+  id: string;
 }
