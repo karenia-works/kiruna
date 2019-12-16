4b7f427801c543fc7338409598c4550dc5baf28c
MaksimXing <hehenryx@buaa.edu.cn>
2019-12-16 17:05:30 +0800

    demo compelete

--- src/pages/personal-page/my-favorite-page/my-favorite-page.module.ts
+++ src/pages/personal-page/my-favorite-page/my-favorite-page.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyFavoritePageComponent } from './my-favorite-page.component';
-
-
+import { BaseComponentsModule } from 'src/components/base-components/base-components.module';
 
 @NgModule({
   declarations: [MyFavoritePageComponent],
-  imports: [
-    CommonModule
-  ],
-  exports: [MyFavoritePageComponent]
+  imports: [CommonModule, BaseComponentsModule],
+  exports: [MyFavoritePageComponent],
 })
-export class MyFavoritePageModule { }
+export class MyFavoritePageModule {}
