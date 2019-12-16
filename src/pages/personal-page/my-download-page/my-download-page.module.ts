ca37587cfededfd26772f43ce3f31ba22f887775
MaksimXing <hehenryx@buaa.edu.cn>
2019-12-16 18:35:16 +0800

    download basic

--- src/pages/personal-page/my-download-page/my-download-page.module.ts
+++ src/pages/personal-page/my-download-page/my-download-page.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyDownloadPageComponent } from './my-download-page.component';
-
-
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [MyDownloadPageComponent],
-  imports: [
-    CommonModule
-  ],
-  exports: [MyDownloadPageComponent]
+  imports: [CommonModule, RouterModule],
+  exports: [MyDownloadPageComponent],
 })
-export class MyDownloadPageModule { }
+export class MyDownloadPageModule {}
