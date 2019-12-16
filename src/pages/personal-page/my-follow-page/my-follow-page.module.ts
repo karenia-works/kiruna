5fdb5eb1a7e0073173df39e1c50c31ea1bc3d168
MaksimXing <hehenryx@buaa.edu.cn>
2019-12-16 18:37:39 +0800

    follow basic

--- src/pages/personal-page/my-follow-page/my-follow-page.module.ts
+++ src/pages/personal-page/my-follow-page/my-follow-page.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyFollowPageComponent } from './my-follow-page.component';
-
-
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [MyFollowPageComponent],
-  imports: [
-    CommonModule
-  ],
-  exports: [MyFollowPageComponent]
+  imports: [CommonModule, RouterModule],
+  exports: [MyFollowPageComponent],
 })
-export class MyFollowPageModule { }
+export class MyFollowPageModule {}
