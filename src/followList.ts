39b485dd28b9185ac878ad75dfd48716f03d27b9
“MaksimXing <henryx@buaa.edu.cn>
2019-12-16 23:55:48 +0800

    follow compelete

--- src/followList.ts
+++ src/followList.ts
@@ -1,49 +1,61 @@
 export const follows = [
   {
+    pic_url:'https://pic3.zhimg.com/a82b51c4284beda5b29d645fa4f91c97_xl.jpg',
     name: '邢惠钧',
     from: '北京航空航天大学'
   },
   {
+    pic_url:'https://pic3.zhimg.com/a82b51c4284beda5b29d645fa4f91c97_xl.jpg',
     name: '祝思凡',
     from: '北京航空航天大学'
   },
   {
+    pic_url:'https://pic3.zhimg.com/a82b51c4284beda5b29d645fa4f91c97_xl.jpg',
     name: '王适意',
     from: '北京航空航天大学'
   },
   {
+    pic_url:'https://pic3.zhimg.com/a82b51c4284beda5b29d645fa4f91c97_xl.jpg',
     name: '李子烨',
     from: '北京航空航天大学'
   },
   {
+    pic_url:'https://pic3.zhimg.com/a82b51c4284beda5b29d645fa4f91c97_xl.jpg',
     name: '刘子暄',
     from: '北京航空航天大学'
   },
   {
+    pic_url:'https://pic3.zhimg.com/a82b51c4284beda5b29d645fa4f91c97_xl.jpg',
     name: '草草',
     from: '北京航空航天大学'
   },
   {
+    pic_url:'https://pic3.zhimg.com/a82b51c4284beda5b29d645fa4f91c97_xl.jpg',
     name: '马老师',
     from: '北京航空航天大学'
   },
   {
+    pic_url:'https://pic3.zhimg.com/a82b51c4284beda5b29d645fa4f91c97_xl.jpg',
     name: 'NSR',
     from: '北京航空航天大学'
   },
   {
+    pic_url:'https://pic3.zhimg.com/a82b51c4284beda5b29d645fa4f91c97_xl.jpg',
     name: '我可以一直：）吗',
     from: '北京航空航天大学'
   },
   {
+    pic_url:'https://pic3.zhimg.com/a82b51c4284beda5b29d645fa4f91c97_xl.jpg',
     name: '孙笑川',
     from: '四川大学'
   },
   {
+    pic_url:'https://pic3.zhimg.com/a82b51c4284beda5b29d645fa4f91c97_xl.jpg',
     name: '孙笑川233',
     from: '四川大学'
   },
   {
+    pic_url:'https://pic3.zhimg.com/a82b51c4284beda5b29d645fa4f91c97_xl.jpg',
     name: '孙笑川NMSL',
     from: '四川大学'
   },
