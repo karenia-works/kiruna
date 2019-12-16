e3ad127477c6ae6745645dd3b614597786551e85
Rynco Maekawa <lynzrand@outlook.com>
2019-12-16 16:42:12 +0800

    Fix enormous amount of bad styles

--- src/pages/professorpage/professorpage.component.ts
+++ src/pages/professorpage/professorpage.component.ts
@@ -11,25 +11,6 @@ import {
   selector: 'app-mainpage',
   templateUrl: './professorpage.component.html',
   styleUrls: ['./professorpage.component.styl'],
-  animations: [
-    trigger('banner-trigger', [
-      transition(':enter', [
-        style({
-          height: '0vh',
-        }),
-        animate(
-          '200ms cubic-bezier(0.0, 0.0, 0.2, 1)',
-          style({ height: '60vh' })
-        ),
-      ]),
-      transition(':leave', [
-        animate(
-          '200ms cubic-bezier(0.4, 0.0, 0.2, 1)',
-          style({ height: '0vh' })
-        ),
-      ]),
-    ]),
-  ],
 })
 export class ProfessorpageComponent implements OnInit {
   constructor() {}
