0a548431cf259f486aff96d1e567909f0a4df2fe
MaksimXing <hehenryx@buaa.edu.cn>
2019-12-16 18:02:42 +0800

    solve routerlink prob

--- src/pages/personal-page/my-favorite-page/my-favorite-page.module.ts
+++ src/pages/personal-page/my-favorite-page/my-favorite-page.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { MyFavoritePageComponent } from './my-favorite-page.component';
 import { BaseComponentsModule } from 'src/components/base-components/base-components.module';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [MyFavoritePageComponent],
-  imports: [CommonModule, BaseComponentsModule],
+  imports: [CommonModule, BaseComponentsModule, RouterModule],
   exports: [MyFavoritePageComponent],
 })
 export class MyFavoritePageModule {}
--- src/pages/personal-page/personal-page.module.ts
+++ src/pages/personal-page/personal-page.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { PersonalPageComponent } from './personal-page.component';
 import { BaseComponentsModule } from 'src/components/base-components/base-components.module';
 
 @NgModule({
   declarations: [PersonalPageComponent],
-  imports: [CommonModule, BaseComponentsModule],
+  imports: [CommonModule, BaseComponentsModule, RouterModule],
   exports: [PersonalPageComponent],
 })
 export class PersonalPageModule {}
